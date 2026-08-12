5cd8cc97d34415a718a31f045e6ba7438f915d42
yeoup9720 <yeoup9720@naver.com>
2026-08-12 14:49:00 +0900

    '모빌리티 정산 서비스'의 결제(payment)시스템

---
+++ src/test/java/com/example/eazyadj/EazyAdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eazyadj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyAdjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
